20fa0cc185425722a4b6ba9fed91bd73faa5c17b
sath-ganp <sath_ganp@tamu.edu>
2019-04-22 21:47:38 -0500

    compile errors fixed

--- max_pool.v
+++ max_pool.v
@@ -1,11 +1,11 @@
-module sum_pool #(parameter window_size = 4)
+module max_pool #(parameter window_size = 4)
   (
     input [window_size*window_size-1:0] [31:0] pool_input  ,
     output [31:0] pool_output
   );
 
-  wire [size*size-2:0][31:0] output_wires;
-  assign pool_output = add_output_wires[size*size-1];
+  wire [window_size*window_size-1:0][31:0] output_wires;
+  assign pool_output = output_wires[window_size*window_size-1];
 
   comparator #(16,32) comp_i0(.a(pool_input[0]),.b(pool_input[1]),.c(output_wires[0]));
 genvar i;
