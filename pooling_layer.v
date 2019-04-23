6a4e7c6d57c167364b65614f8cabb8d48623b6fd
sath-ganp <sath_ganp@tamu.edu>
2019-04-22 21:46:27 -0500

    changes to input and compile errors fixed

--- pooling_layer.v
+++ pooling_layer.v
@@ -1,9 +1,9 @@
 module pooling_layer #(parameter input_size, parameter pooling_size)
 (
-  input [input_size*input_size-1:0] input_data,
-  /*input [31:0] pooling_multiplier*/,
-  output [((input_size/pooling_size)*(input_size/pooling_size))-1:0][31:0] pooling_output;
+  input [input_size*input_size-1:0] [31:0] input_data,
+  output [((input_size/pooling_size)*(input_size/pooling_size))-1:0][31:0] pooling_output
  );
+  
 genvar i;
 genvar j;
 genvar k;
@@ -19,12 +19,12 @@ generate
         for(l=j;l<(j+pooling_size);l=l+1)
         begin
           wire [31:0] multiplied_output;
-          // qmult #(15,32) qmult_i(.i_multiplicand(pooling_multiplier),.i_multiplier(input_data[(k*input_size)+l]),.o_result(multiplied_output));
-          assign sub_input_data[(k-i)*(pooling_size)+(l-j)] = /*multiplied_output*/ input_data[(k*input_size)+l];
+          assign sub_input_data[(k-i)*(pooling_size)+(l-j)] = input_data[(k*input_size)+l];
         end
       end
       max_pool #(.window_size(pooling_size)) max_pool_i(.pool_input(sub_input_data),.pool_output(pooling_output[((i/pooling_size)*(input_size/pooling_size))+(j/pooling_size)]));
     end
   end
 endgenerate
+  
 endmodule
